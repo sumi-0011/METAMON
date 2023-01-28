82ae109801d61c5f5c81df094f21a0b69efe4e3b
sumi-0011 <selina2000@naver.com>
2023-01-29 03:16:55 +0900

    feat: section-3 add

--- src/components/pages/onboarding/index.tsx
+++ src/components/pages/onboarding/index.tsx
@@ -1,6 +1,7 @@
 import MainSection from '@/components/pages/onboarding/main-section';
 import Section1 from '@/components/pages/onboarding/section-1';
 import Section2 from '@/components/pages/onboarding/section-2';
+import Section3 from '@/components/pages/onboarding/section-3';
 import styled from 'styled-components';
 
 function Onboarding() {
@@ -8,8 +9,10 @@ function Onboarding() {
     <Wrapper>
       <MainSection />
       <Section1 />
-      <hr />
+      <Divider />
       <Section2 />
+      <Divider />
+      <Section3 />
     </Wrapper>
   );
 }
@@ -20,6 +23,9 @@ const Title = styled.h1`
   font-size: 30px;
 `;
 
+const Divider = styled.hr`
+  margin-top: 150px;
+`;
 const Wrapper = styled.div`
   width: 100%;
   margin: auto;
